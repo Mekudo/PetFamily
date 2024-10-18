b77e2cb639dcb08114df2d447968b30997d4052d
Mekudo <maksimkamaksimka072825@gmail.com>
2024-10-18 18:47:51 +0300

    ID-B-4

---
+++ PetFamily.Backend/src/PetFamily.Domain/Models/ModelPet/ValueObjects/PetPhoto.cs
@@ -0,0 +1,26 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace PetFamily.Domain.Models.ModelPet.ValueObjects;
+
+public record PetPhoto
+{
+    private string Path { get; }
+    
+    private bool IsMainPhoto { get; }
+
+    private PetPhoto(string path, bool isMainPhoto)
+    {
+        Path = path;
+        IsMainPhoto = isMainPhoto;
+    }
+
+    public static Result<PetPhoto> Create(string path, bool isMainPhoto)
+    {
+        if (string.IsNullOrWhiteSpace(path))
+            return Result.Failure<PetPhoto>("Путь к фото не указан");
+        
+        var petPhoto = new PetPhoto(path, isMainPhoto);
+        
+        return Result.Success(petPhoto);
+    }
+};
