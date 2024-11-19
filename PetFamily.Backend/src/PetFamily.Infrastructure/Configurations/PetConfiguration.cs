12d9e05b859caaa7a3c593209a95ce0ba632b6a9
Mekudo <maksimkamaksimka072825@gmail.com>
2024-11-20 01:05:48 +0300

    ID-B-4.1

---
+++ PetFamily.Backend/src/PetFamily.Infrastructure/Configurations/PetConfiguration.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetFamily.Domain.Models.ModelPet;
+
+namespace PetFamily.Infrastructure.Configurations;
+
+public class PetConfiguration: IEntityTypeConfiguration<Pet>
+{
+    public void Configure(EntityTypeBuilder<Pet> builder)
+    {
+        builder.ToTable("Pets");
+        
+        builder.HasKey(p => p.Id);
+
+        builder.Property(p => p.Name)
+            .HasColumnName("name")
+            .HasMaxLength(50)
+            .IsRequired();
+        
+        builder.Property(p => p.Species)
+            .HasColumnName("species")
+            .HasMaxLength(50)
+            .IsRequired();
+        
+        builder.Property(p => p.Description)
+            .HasColumnName("description")
+            .HasMaxLength(300)
+            .IsRequired();
+        
+        builder.Property(p => p.Breed)
+            .HasColumnName("breed")
+            .HasMaxLength(50)
+            .IsRequired();
+        
+        builder.Property(p => p.Color)
+            .HasColumnName("color")
+            .HasMaxLength(50)
+            .IsRequired();
+        
+        builder.Property(p => p.HealthInfo)
+            .HasColumnName("health_info")
+            .HasMaxLength(100)
+            .IsRequired();
+        
+        builder.Property(p => p.Address)
+            .HasColumnName("address")
+            .HasMaxLength(100)
+            .IsRequired();
+        
+        builder.Property(p => p.Weight)
+            .HasColumnName("weight")
+            .HasMaxLength(30)
+            .IsRequired();
+        
+        builder.Property(p => p.Height)
+            .HasColumnName("height")
+            .HasMaxLength(30)
+            .IsRequired();
+        
+        builder.Property(p => p.PhoneNumber)
+            .HasColumnName("phone_number")
+            .HasMaxLength(30)
+            .IsRequired();
+        
+        builder.Property(p => p.PhoneNumber)
+            .HasColumnName("phone_number")
+            .HasMaxLength(30)
+            .IsRequired();
+        
+        builder.Property(p => p.IsCastrated)
+            .HasColumnName("is_castrated")
+            .HasMaxLength(15)
+            .IsRequired();
+        
+        builder.Property(p => p.DateOfBirth)
+            .HasColumnName("date_of_birth")
+            .HasMaxLength(50)
+            .IsRequired();
+        
+        builder.Property(p => p.IsVaccinated)
+            .HasColumnName("is_vaccinated")
+            .HasMaxLength(15)
+            .IsRequired();
+        
+        builder.Property(p => p.SupportStatus)
+            .HasColumnName("support_status")
+            .HasMaxLength(15)
+            .IsRequired();
+
+        builder.OwnsMany(p => p.BankRequisites, br =>
+        {
+            br.ToJson();
+            
+            br.Property(b => b.NameOfBank)
+                .IsRequired();
+            br.Property(b => b.BankIdentificationCode)
+                .IsRequired();
+            br.Property(b => b.CorrespondentAccount)
+                .IsRequired();
+        });
+        
+        builder.Property(p => p.DateOfCreated)
+            .HasColumnName("date_of_created")
+            .HasMaxLength(50)
+            .IsRequired();
+
+        builder.OwnsMany(p => p.PetPhotos, pf =>
+        {
+            pf.ToJson();
+            
+            pf.Property(ph => ph.Path)
+                .IsRequired();
+            
+            pf.Property(imp => imp.IsMainPhoto)
+                .IsRequired();
+        });
+    }
+}
---
+++ PetFamily.Backend/src/PetFamily.Infrastructure/Configurations/VolunteerConfiguration.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetFamily.Domain.Models.ModelVolunteer;
+
+namespace PetFamily.Infrastructure.Configurations;
+
+public class VolunteerConfiguration : IEntityTypeConfiguration<Volunteer>
+{
+    public void Configure(EntityTypeBuilder<Volunteer> builder)
+    {
+        builder.ToTable("Volunteers");
+        
+        builder.HasKey(v => v.Id);
+
+        builder.OwnsOne(v => v.FIO, fo =>
+        {
+            fo.ToJson();
+
+            fo.Property(fio => fio.Name)
+                .HasMaxLength(50)
+                .IsRequired();
+            fo.Property(fio => fio.MiddleName)
+                .HasMaxLength(50)
+                .IsRequired();
+            fo.Property(fio => fio.SurName)
+                .HasMaxLength(50)
+                .IsRequired();
+        });
+        
+        builder.Property(v => v.Email)
+            .HasColumnName("email")
+            .HasMaxLength(100)
+            .IsRequired();
+        
+        builder.Property(v => v.Description)
+            .HasColumnName("description")
+            .HasMaxLength(300)
+            .IsRequired();
+        
+        builder.Property(v => v.WorkExperience)
+            .HasColumnName("work_experience")
+            .IsRequired();
+        
+        builder.Property(v => v.PhoneNumber)
+            .HasColumnName("phone_number")
+            .HasMaxLength(50)
+            .IsRequired();
+
+        builder.OwnsMany(v => v.SocialNetwork, sn =>
+        {
+            sn.ToJson();
+            
+            sn.Property(soc => soc.Title)
+                .HasMaxLength(50)
+                .IsRequired();
+            sn.Property(soc => soc.Link)
+                .HasMaxLength(50)
+                .IsRequired();
+        });
+        
+        builder.OwnsMany(v => v.BankRequisites, br =>
+        {
+            br.ToJson();
+            
+            br.Property(b => b.NameOfBank)
+                .IsRequired();
+            br.Property(b => b.BankIdentificationCode)
+                .IsRequired();
+            br.Property(b => b.CorrespondentAccount)
+                .IsRequired();
+        });
+        
+        builder.HasMany(v => v.Pets)
+            .WithOne()
+            .HasForeignKey("volunteer_id");
+    }
+}
